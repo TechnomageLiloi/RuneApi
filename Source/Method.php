c3fc83f6b446c67f4c575bd79a2ad7ff6d9ccb02
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-02-06 01:07:20 +0200

    [0.0.3] Add response methods.

--- Source/Method.php
+++ Source/Method.php
@@ -4,13 +4,24 @@ namespace Liloi\API;
 
 class Method
 {
+    protected string $name;
+
+    protected $f;
+
+    public function __construct(string $name, callable $f)
+    {
+        $this->name = $name;
+        $this->f = $f;
+    }
+
     public function getName(): string
     {
-
+        return $this->name;
     }
 
     public function execute(array $parameters): Response
     {
-
+        $call = $this->f;
+        return $call($parameters);
     }
 }
--- Source/Response.php
+++ Source/Response.php
@@ -4,5 +4,20 @@ namespace Liloi\API;
 
 class Response
 {
+    private array $list = [];
 
+    public function set(string $name, $value): void
+    {
+        $this->list[$name] = $value;
+    }
+
+    public function get(): array
+    {
+        return $this->list;
+    }
+
+    public function getAsJSON(): string
+    {
+        return json_encode($this->list, JSON_UNESCAPED_UNICODE);
+    }
 }
